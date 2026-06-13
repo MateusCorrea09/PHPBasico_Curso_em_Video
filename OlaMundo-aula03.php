2cbb6ece9ce81ae46331c3bc3f1953c6406d24e2
MateusCorrea09 <mateus.contato09@outlook.com>
2026-06-13 11:24:25 -0300

    Aula 01 - 03 PHP

---
+++ OlaMundo-aula03.php
@@ -0,0 +1,14 @@
+<!DOCTYPE>
+<html>
+    <head>
+        <meta charset = "UTF-8"/>
+        <title>Oi! aqui é um titulo</title>
+    <body>
+        <h1>TESTE! aki é o corpo!</h1>
+    
+        <?php 
+            echo "Olá mundo";
+        ?>>
+    </body>
+    </head>
+</html>
