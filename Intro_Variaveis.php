97a3e144b2a014e41ef8cf2e627a3c989a5f47f3
MateusCorrea09 <mateus.contato09@outlook.com>
2026-06-14 09:59:00 -0300

    Aulas 04 - 05

---
+++ Intro_Variaveis.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset = "UTF-8">
+        <link rel="stylesheet" href="_css/estilo.css"/>
+        <title>Introdução a variáveis</title>
+    </head>
+    <body>
+        <div>
+        
+            <?php
+                //Para criar comentários devemos usar "//"
+                // ou podemos usar /* [...] */
+                $n = 5;
+                $no = "Mateus";
+                echo $no;
+            ?>
+        </div>  
+    </body>
+</html>
---
+++ operadores_ari.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <title>Operadores Aritiméticos</title>
+        <link  ref="stylesheet" href="_css/estilo.css"/>
+    </head>
+    <body>
+        <div>
+            <?php
+                $n1 = 3;
+                $n2 = 2;
+                $s = $n1 + $n2;
+                echo "A soma de [$n1] e [$n2] é [$s]";
+            ?>
+        </div>
+    </body>
+</html>
